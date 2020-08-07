c8eea637d22b7dfcd7e52d460cb40f2253a9de4f
JohnHynes <johnthehynes@gmail.com>
2020-08-07 13:54:56 -0400

    echo command

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 
-
 class BasicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,6 +8,11 @@ class BasicCog(commands.Cog):
     async def on_message(self, message):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
+
+    @commands.command(name='echo')
+    async def on_message(self, message):
+        msg = '{0.message.content}'.format(message)
+        await message.channel.send(msg[5:])
 
 
 
